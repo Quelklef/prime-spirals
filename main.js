f31d496ee80e96b80a1ee2fb04ba4522ce73ddd3
Maynard <elimaynard923@gmail.com>
2021-08-14 23:28:15 -0700

    automatically frame the result drawing

--- main.js
+++ main.js
@@ -46,15 +46,35 @@ let isPrime;
   }
 }
 
-// calculates the physical path of primes, represented as a generator of [x,y] coordinates
-function* calcPath({ bound, turnAngle }) {
+// calculates the physical path of primes, represented as an array of [x,y] coordinates
+function calcPath({ bound, turnAngle }) {
+  const path = [];
+
+  let minX = Infinity;
+  let maxX = -Infinity;
+  let minY = Infinity;
+  let maxY = -Infinity;
+  let sumX = 0;
+  let cntX = 0;
+  let sumY = 0;
+  let cntY = 0;
+
   let x = 0.0;
   let y = 0.0;
   let theta = Math.PI / 2;
-
   let prevprime = 0n;
+
   for (let n = 1n; n <= bound + 1n; n++) {
-    yield [x, y];
+    path.push([x, y]);
+
+    minX = Math.min(x, minX);
+    maxX = Math.max(x, maxX);
+    minY = Math.min(y, minY);
+    maxY = Math.max(y, maxY);
+    sumX += x;
+    cntX += 1;
+    sumY += y;
+    cntY += 1;
 
     if (isPrime(n) || n === bound) {
       x += Math.cos(theta) * Number(n - prevprime);
@@ -64,25 +84,46 @@ function* calcPath({ bound, turnAngle }) {
     }
   }
 
+  return {
+    path,
+    summ: {
+      minX, maxX, rangeX: maxX - minX, avgX: sumX / cntX,
+      minY, maxY, rangeY: maxY - minY, avgY: sumY / cntY,
+    },
+  };
+
   function mod(x, n) {
     return ((x % n) + n) % n;
   }
 }
 
 function render(c, state) {
+  const { path, summ } = calcPath(state);
+
+  // maps a physical coordinate to a pixel coordinate
+  let T;
+  {
+    const scale = Math.min(
+      1 / summ.rangeX * state.canvasWidth,
+      1 / summ.rangeY * state.canvasHeight,
+    );
+    const phyOffsetX = -summ.minX;
+    const phyOffsetY = -summ.minY;
+    const pixOffsetX = (state.canvasWidth - summ.rangeX * scale) / 2;
+    const pixOffsetY = (state.canvasHeight - summ.rangeY * scale) / 2;
+    T = (x, y) =>
+      [ (x + phyOffsetX) * scale + pixOffsetX
+      , (y + phyOffsetY) * scale + pixOffsetY
+      ];
+  }
+
   c.fillStyle = 'black';
   c.beginPath();
   c.moveTo(...T(0, 0));
-  for (const [x, y] of calcPath(state)) {
+  for (const [x, y] of path) {
     c.lineTo(...T(x, y));
   }
   c.stroke();
-
-  function T(x, y) {
-    return [ (x + state.panX) * state.scale + state.canvasWidth / 2
-           , (y + state.panY) * state.scale + state.canvasHeight / 2
-           ];
-  }
 }
 
 
@@ -105,9 +146,6 @@ function main() {
       turnAngle: Number($optAngle.value) * Math.PI,
       canvasWidth: $canv.width,
       canvasHeight: $canv.height,
-      panX: 0.0,
-      panY: 0.0,
-      scale: 3.0,
     };
 
     const ctx = $canv.getContext('2d');
